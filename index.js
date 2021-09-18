76b4408fe94b9fe5f0099359207b4a3a41b9a7d8
Robin Correa <robin.correa21@gmail.com>
2021-09-18 10:37:53 +0800

    add routes and restructure applications

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const morgan = require("morgan");
 
 const logger = require("./logger");
 
+const courses = require("./routes/courses");
+
 const express = require("express");
 
 const app = express();
@@ -36,79 +38,13 @@ if (process.env.NODE_ENV === "development") {
 // Logger middleware (custom)
 app.use(logger);
 
-// Records
-const courses = [
-  { id: 1, name: "Course 1" },
-  { id: 2, name: "Course 2" },
-];
+// Courses route
+app.use("/api/courses", courses);
 
 // Main entry point
 app.get("/", (req, res) => {
   res.render("index", { title: "My Express App", message: "Hello World" });
 });
-
-// Get all records
-app.get("/api/courses", (req, res) => {
-  res.send(courses);
-});
-
-// Get one record
-app.get("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course)
-    return res.status(404).send("The course with the given ID was not found.");
-
-  res.send(course);
-});
-
-// Create record
-app.post("/api/courses", (req, res) => {
-  const { error } = validateCourse(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name,
-  };
-
-  courses.push(course);
-
-  res.send(course);
-});
-
-// Update record
-app.put("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course)
-    return res.status(404).send("The course with the given ID was not found.");
-
-  const { error } = validateCourse(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  course.name = req.body.name;
-
-  res.send(course);
-});
-
-// Delete record
-app.delete("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course)
-    return res.status(404).send("The course with the given ID was not found.");
-
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-
-  res.send(course);
-});
-
-function validateCourse(course) {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-
-  return schema.validate(course);
-}
 
 const port = process.env.PORT || 3000;
 
---
+++ routes/courses.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const router = express.Router();
+
+// Records
+const courses = [
+  { id: 1, name: "Course 1" },
+  { id: 2, name: "Course 2" },
+];
+
+// Get all records
+router.get("/", (req, res) => {
+  res.send(courses);
+});
+
+// Get one record
+router.get("/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found.");
+
+  res.send(course);
+});
+
+// Create record
+router.post("/", (req, res) => {
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+
+  courses.push(course);
+
+  res.send(course);
+});
+
+// Update record
+router.put("/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found.");
+
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  course.name = req.body.name;
+
+  res.send(course);
+});
+
+// Delete record
+router.delete("/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found.");
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(course);
+}
+
+module.exports = router;
